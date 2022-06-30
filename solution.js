ac32bb9c367e87af5e050864235f775d4f9e724d
xstartstaticx <daniel.reiller@icloud.com>
2022-06-30 15:25:27 +0200

    finished project

--- solution.js
+++ solution.js
@@ -1,1 +1,71 @@
 // write your code in this file
+console.log("Q1:", 6450 / 15, "tickets are sold");
+
+console.log("Q2:", 500 * 52.1429, "dollars per year");
+
+console.log("Q3:", 0.17 * 30, "%");
+
+// const side = "4.75";
+// const perimeterSquare = side * 4;
+// console.log("Q4:", perimeterSquare, "cm");
+
+console.log("Q4:", 4.75 * 4, "cm");
+
+// console.log("Q5:", 5 + 6 + 7, "cm");
+console.log(`Q5: ${5 + 6 + 7} cm`);
+
+console.log("Q6:", 5 * 5, "cm²");
+
+console.log(`Q7: ${(3 * 4) / 2} cm²`);
+
+console.log(`Q8: ${9 * 9 * 9} cm³`);
+
+const bill1 = 22.35 * 1.1;
+const bill2 = 26.67 * 1.15;
+const bill3 = 35.92 * 1.2;
+console.log(
+  `Q9: the 3 bills: 
+  ${bill1}, ${bill2}, and ${bill3}.`
+);
+
+console.log(
+  `Q10: The average hours worked per day is: ${
+    (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10
+  } hours per day`
+);
+
+// let testScore1 = 75;
+// let testScore2 = 70;
+// let testScore3 = 85;
+// let testScore4 = 90;
+// let testScore5 = 100;
+// testScore6 = x
+// const currentAverage =
+//   (testScore1 + testScore2 + testScore3 + testScore4 + testScore5) / 5;
+// const newAverage = 85;
+// console.log(currentAverage);
+// let x be the 6th score
+// (75 + 70 +85 + 90 + 100 + x) / 6 = 85
+//  x = 85 * 6 -(75 + 70 +85 + 90 + 100)
+console.log(
+  `Q11: Score in the sixth test: ${85 * 6 - (75 + 70 + 85 + 90 + 100)}.`
+);
+
+/**
+ * 80
+ * 9
+ *
+ * 8
+ * 78
+ * let y be the sum of the first 8 scores
+ * y/8 = 78
+ * y = 78*8 = the sum of the first 8 tests
+ *
+ * let x be the minimum he needs
+ * (78*8 + x) / 9 = 80
+ * x = 80*9-(78*8)
+ *
+ */
+console.log(
+  `Q12: James needs a minimum of ${80 * 9 - 78 * 8}% to get an 80% average.`
+);
